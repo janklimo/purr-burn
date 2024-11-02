b1ee8147ea2ff780c2b75d74b7f62548037d29d7
Jan Klimo <jan.klimo@gmail.com>
2024-11-02 12:45:24 +0700

    Remove donation adddress

--- src/components/TradeCallout.tsx
+++ src/components/TradeCallout.tsx
@@ -8,7 +8,10 @@ import TradeButton from '@/components/TradeButton';
 
 const TradeCallout: FC = () => (
   <div className='flex flex-col justify-center items-center text-center px-2'>
-    <div className='mt-6 mb-6'>
+    <div className='mt-2'>
+      <Friends />
+    </div>
+    <div className='my-6'>
       <TradeButton />
       <p className='text-gray-400 text-sm mt-3'>
         Using this referral link gives you a{' '}
@@ -23,16 +26,7 @@ const TradeCallout: FC = () => (
         on your trading fees.
       </p>
     </div>
-    <div>
-      <p className='text-white text-sm font-semibold mb-1'>
-        For tips on Hyperliquid L1
-      </p>
-      <p className='text-hlGray font-mono text-xs mb-8'>
-        0xbf8802002D394F6f53d1c5a5Cc500Fd8DFa3B7C8
-      </p>
-    </div>
-    <Friends />
-    <div className='text-white mt-5 text-center text-sm'>
+    <div className='text-white mt-2 text-center text-sm'>
       By{' '}
       <a
         href='https://twitter.com/janklimo'
