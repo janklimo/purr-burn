4b5d200a8f24ee8daf060c818991e733dd5e25e1
Jan Klimo <jan.klimo@gmail.com>
2024-11-09 22:30:53 +0700

    Blur placeholders

--- src/app/make-a-difference/Gallery.tsx
+++ src/app/make-a-difference/Gallery.tsx
@@ -1,33 +1,45 @@
 import Image from 'next/image';
 
+import IMG_1928 from '/public/images/gallery/IMG_1928.jpg';
+import IMG_1943 from '/public/images/gallery/IMG_1943.jpg';
+import IMG_1951 from '/public/images/gallery/IMG_1951.jpg';
+import IMG_1957 from '/public/images/gallery/IMG_1957.jpg';
+import IMG_2087 from '/public/images/gallery/IMG_2087.jpg';
+import IMG_2152 from '/public/images/gallery/IMG_2152.jpg';
+import IMG_2168 from '/public/images/gallery/IMG_2168.jpg';
+import IMG_2176 from '/public/images/gallery/IMG_2176.jpg';
+import IMG_2212 from '/public/images/gallery/IMG_2212.jpg';
+import IMG_2228 from '/public/images/gallery/IMG_2228.jpg';
+
 const Gallery = () => {
   const images = [
-    { src: '/images/gallery/IMG_1951.jpg', alt: 'Cat' },
-    { src: '/images/gallery/IMG_1943.jpg', alt: 'Cat' },
-    { src: '/images/gallery/IMG_1928.jpg', alt: 'Cat' },
-    { src: '/images/gallery/IMG_1957.jpg', alt: 'Cat' },
-    { src: '/images/gallery/IMG_2087.jpg', alt: 'Cat' },
-    { src: '/images/gallery/IMG_2168.jpg', alt: 'Cat' },
-    { src: '/images/gallery/IMG_2176.jpg', alt: 'Cat' },
-    { src: '/images/gallery/IMG_2152.jpg', alt: 'Cat' },
-    { src: '/images/gallery/IMG_2212.jpg', alt: 'Cat' },
-    { src: '/images/gallery/IMG_2228.jpg', alt: 'Cat' },
+    { image: IMG_1951, alt: 'Cat' },
+    { image: IMG_1943, alt: 'Cat' },
+    { image: IMG_1928, alt: 'Cat' },
+    { image: IMG_1957, alt: 'Cat' },
+    { image: IMG_2087, alt: 'Cat' },
+    { image: IMG_2168, alt: 'Cat' },
+    { image: IMG_2176, alt: 'Cat' },
+    { image: IMG_2152, alt: 'Cat' },
+    { image: IMG_2212, alt: 'Cat' },
+    { image: IMG_2228, alt: 'Cat' },
   ];
 
   return (
     <div className='container mx-auto px-4 py-8'>
       <div className='grid grid-cols-2 lg:grid-cols-5 gap-4'>
-        {images.map((image) => {
+        {images.map((item) => {
           return (
             <div
-              key={image.src}
+              key={item.image.src}
               className='relative rounded-lg overflow-hidden group'
               style={{ height: '350px' }}
             >
               <Image
-                src={image.src}
-                alt={image.alt}
+                src={item.image}
+                alt={item.alt}
                 fill
+                placeholder='blur'
                 className='object-cover transition-transform duration-300 group-hover:scale-105'
                 sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
               />
