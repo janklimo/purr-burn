c557d983abca6f46a442bd05c48abe42a8e8b223
Jan Klimo <jan.klimo@gmail.com>
2024-11-21 21:00:39 +0700

    Links to shelter

--- src/app/make-a-difference/Shelter.tsx
+++ src/app/make-a-difference/Shelter.tsx
@@ -1,13 +1,25 @@
 import { FC } from 'react';
 
+import UnderlineLink from '@/components/links/UnderlineLink';
+
 const Shelter: FC = () => {
   return (
     <>
-      <p className='text-hlGray text-sm text-center mb-8'>Background</p>
+      <p className='text-hlGray text-sm text-center mb-8'>
+        Their home is civic organization{' '}
+        <UnderlineLink href='https://zilinskelabky.sk/' className='mr-2'>
+          Žilinské labky ️🐱
+        </UnderlineLink>
+        located in{' '}
+        <UnderlineLink href='https://maps.app.goo.gl/VMqBQgEBBzYW5vNQ6'>
+          Žilina, Slovakia
+        </UnderlineLink>
+        .
+      </p>
       <div className='flex justify-center mb-6'>
         <iframe
-          width='560'
-          height='315'
+          width='672'
+          height='378'
           src='https://www.youtube.com/embed/25YId7nEmmw?si=yowUtpAGKcafTl3H&cc_lang_pref=en&cc_load_policy=1'
           title='YouTube video player'
           frameBorder='0'
