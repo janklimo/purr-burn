6161dff7c39ece4e651ad37cce8f817b4caf8454
Jan Klimo <jan.klimo@gmail.com>
2024-11-23 18:56:07 +0700

    Finish intro

--- src/app/make-a-difference/Shelter.tsx
+++ src/app/make-a-difference/Shelter.tsx
@@ -5,17 +5,27 @@ import UnderlineLink from '@/components/links/UnderlineLink';
 const Shelter: FC = () => {
   return (
     <>
-      <p className='text-hlGray text-sm text-center mb-8'>
-        Their home is civic organization{' '}
-        <UnderlineLink href='https://zilinskelabky.sk/' className='mr-2'>
-          Žilinské labky ️🐱
-        </UnderlineLink>
-        located in{' '}
-        <UnderlineLink href='https://maps.app.goo.gl/VMqBQgEBBzYW5vNQ6'>
-          Žilina, Slovakia
-        </UnderlineLink>
-        .
-      </p>
+      <div className='max-w-2xl mx-auto text-hlGray text-center text-sm'>
+        <p className='my-6'>
+          The cats call{' '}
+          <UnderlineLink href='https://zilinskelabky.sk/' className='mr-2'>
+            Žilinské labky ️🐱
+          </UnderlineLink>
+          their home - a shelter located in{' '}
+          <UnderlineLink href='https://maps.app.goo.gl/VMqBQgEBBzYW5vNQ6'>
+            Žilina, Slovakia
+          </UnderlineLink>
+          .
+        </p>
+        <p className='my-6'>
+          Every day, the voluteers at Žilinské labky fight to give stray cats a
+          second chance at life. The shelter is their sanctuary - but right now,
+          its leaking roof threatens their safety. Help them repair their home
+          so that they can continue protecting these vulnerable souls. Every
+          PURR makes a difference.
+        </p>
+        <p className='my-6'>See their journey:</p>
+      </div>
       <div className='flex justify-center mb-6'>
         <iframe
           width='672'
