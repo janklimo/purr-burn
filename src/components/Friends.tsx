f223a0ab32e101ba1a0fd8b701083bea7aa4e587
Jan Klimo <jan.klimo@gmail.com>
2024-12-07 12:45:55 +0700

    Add HypurrCollective

--- src/components/Friends.tsx
+++ src/components/Friends.tsx
@@ -6,6 +6,16 @@ import { FC } from 'react';
 
 const friends = [
   {
+    name: 'HypurrCollective',
+    link: 'https://www.hypurr.co/',
+    logo: '/images/friends/hypurrcollective.jpeg',
+  },
+  {
+    name: 'HypurrScan',
+    link: 'https://hypurrscan.io/',
+    logo: '/images/friends/hypurrscan.png',
+  },
+  {
     name: 'HyBridge',
     link: 'https://hybridge.xyz/',
     logo: '/images/friends/hybridge.svg',
@@ -14,11 +24,6 @@ const friends = [
     name: 'HyperDATA',
     link: 'https://hyperscan.fun/',
     logo: '/images/friends/hyperfun.svg',
-  },
-  {
-    name: 'HypurrScan',
-    link: 'https://hypurrscan.io/',
-    logo: '/images/friends/hypurrscan.png',
   },
 ];
 
@@ -35,14 +40,15 @@ const Friends: FC = () => (
             className='group relative flex border border-hl-light cursor-pointer rounded-lg bg-white/5 py-2 px-3 shadow-md transition hover:bg-primary-200/20 hover:border hover:border-hl-primary'
           >
             <a href={friend.link} target='_blank' rel='noopener noreferrer'>
-              <div className='flex justify-center w-full text-sm/6'>
-                <div className='w-5 inline-block mr-2'>
+              <div className='flex justify-center items-center w-full'>
+                <div className='w-6 inline-block mr-2'>
                   <Image
                     src={friend.logo}
                     width={64}
                     height={64}
                     priority
                     alt={friend.name}
+                    className='rounded-full'
                   />
                 </div>
                 <span className='font-semibold text-gray-300 group-hover:text-white text-sm'>
