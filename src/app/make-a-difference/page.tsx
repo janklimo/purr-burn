942889761b85c0153df2e441f8743699b926dabd
Jan Klimo <jan.klimo@gmail.com>
2024-12-24 17:49:27 +0700

    Link to ToC

--- src/app/make-a-difference/page.tsx
+++ src/app/make-a-difference/page.tsx
@@ -3,6 +3,7 @@
 import { FC } from 'react';
 
 import CopyToClipboard from '@/components/CopyToClipboard';
+import UnderlineLink from '@/components/links/UnderlineLink';
 import TradeCallout from '@/components/TradeCallout';
 
 import Boxes from '@/app/make-a-difference/Boxes';
@@ -34,13 +35,14 @@ const Campaign: FC = () => {
         </div>
 
         <div className='my-10 md:my-16'>
-          <h2 className='font-serif text-white text-3xl mb-3 text-center'>
+          <h2 className='font-serif text-white text-3xl mb-5 text-center'>
             How to donate?
           </h2>
-          <p className='text-hlGray text-center text-sm mb-5 px-5'>
-            Transfer PURR to this address:
-          </p>
           <CopyToClipboard />
+          <p className='text-hlGray text-center text-sm mt-2 px-5'>
+            Send PURR to this address. By doing so, you agree to our{' '}
+            <UnderlineLink href='/terms'>Terms of Use</UnderlineLink>.
+          </p>
         </div>
 
         <div className='my-10 md:my-16'>
