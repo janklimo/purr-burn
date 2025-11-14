93aca9b825e4bf1fa5a60054a589fadaf4d1aa3b
Jan Klimo <jan.klimo@gmail.com>
2025-11-14 13:30:02 +0700

    Update stake button link

--- src/components/StakeButton.tsx
+++ src/components/StakeButton.tsx
@@ -4,7 +4,7 @@ import { FC } from 'react';
 const StakeButton: FC = () => {
   return (
     <a
-      href='https://www.stakingrewards.com/stake-app?input=hyperliquid&best-deal=true&validator=purrposefulnode&locked=true'
+      href='https://app.loopingcollective.org/product/lhype?partner=purrposefulnode'
       className='inline-flex items-center bg-beige text-sm text-yellow-900 px-3 py-2 hover:bg-beige-hover transition-all duration-300 rounded-md'
       target='_blank'
       rel='noopener noreferrer'
